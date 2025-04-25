68d8faa6df658f99c3573d92bc93ee79e82622fc
paulofranklins <paulofranklinc@gmail.com>
2025-04-25 15:01:38 -0700

    feat(ui): add ClearScreen method to creat a clean transaction from one menu screen to another.

---
+++ src/main/java/com/pluralsight/ClearScreen.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ClearScreen {
+    public void clearScreen() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
+    }
+}
