ba5cf7dfa2e86800b573468ab8cbf718fcfe4cda
paulofranklins <paulofranklinc@gmail.com>
2025-04-25 15:05:12 -0700

    refactor: extracted mainMenu logic from AccountingLedgerApp, and moved to MainMenu.java

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,21 +1,9 @@
 package com.pluralsight;
 
 public class AccountingLedgerApp {
-    private final Menu menu = new Menu();
+    private final MainMenu menu = new MainMenu();
 
     public void start() {
-        while (true) {
-            var option = menu.showMainMenu();
-            switch (option) {
-                case "D" -> System.out.println("Add Deposit");
-                case "P" -> System.out.println("Make Payment");
-                case "L" -> System.out.println("Ledger");
-                case "X" -> {
-                    System.out.println("Thank you for using accounting ledger app");
-                    System.exit(0);
-                }
-                default -> System.out.println("Invalid option");
-            }
-        }
+        menu.mainMenuLogic();
     }
 }
