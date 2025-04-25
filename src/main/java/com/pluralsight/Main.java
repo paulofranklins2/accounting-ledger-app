c15fcf0ddf64e94a3b43d4c3a5582f3e0465164e
paulofranklins <paulofranklinc@gmail.com>
2025-04-25 16:41:23 -0700

    refactor: moved final classes to main at first instance

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
+    public static final HomeScreen homeScreen = new HomeScreen();
+    public static final ClearScreen clearScreen = new ClearScreen();
+    public static final TransactionService transactionService = new TransactionService();
+    public static final LedgerScreen ledgerScreen = new LedgerScreen();
+    public static final ReportScreen reportScreen = new ReportScreen();
+
     public static void main(String[] args) {
 
         new AccountingLedgerApp().start();
