72d8608bb4499c730d298c14882e0300ffcc5178
paulofranklins <paulofranklinc@gmail.com>
2025-04-25 16:39:10 -0700

    feat: implemented ReportScreen menu and methods showReportMenu and reportMenuLogic

---
+++ src/main/java/com/pluralsight/ReportScreen.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+import static com.pluralsight.Main.*;
+
+public class ReportScreen {
+
+
+    private String showReportMenu() {
+        System.out.println("Report Options");
+        System.out.println("[1] Month To Date");
+        System.out.println("[2] Previous Month");
+        System.out.println("[3] Year To Date");
+        System.out.println("[4] Previous Year");
+        System.out.println("[5] Search by Vendor - prompt the user for the vendor name");
+        System.out.println("[6] Custom Search");
+        System.out.println("[0] Back - Go back to the report page");
+        System.out.println("[H] Home - Go back to the home page");
+
+        System.out.print("Select a report option: ");
+        return scanner.nextLine().trim().toUpperCase();
+    }
+
+    public void reportMenuLogic() {
+        while (true) {
+            String s = showReportMenu();
+            switch (s) {
+                case "1" -> System.out.println("Month To Date");
+                case "2" -> System.out.println("Previous Month");
+                case "3" -> System.out.println("Year To Date");
+                case "4" -> System.out.println("Previous Year");
+                case "5" -> System.out.println("Search by Vendor - prompt the user for the vendor");
+                case "6" -> System.out.println("Custom Search");
+                case "0" -> {
+                    System.out.println("Report - Go back to the report page");
+                    ledgerScreen.ledgerMenuLogic();
+                }
+                case "H" -> {
+                    System.out.println("Home - Go back to the home page");
+                    homeScreen.mainMenuLogic();
+                }
+                default -> System.out.println("Select an option");
+            }
+        }
+    }
+
+}
