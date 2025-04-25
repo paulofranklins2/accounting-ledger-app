88c834099738a20cef64f9c3c432ea3258bc1a53
paulofranklins <paulofranklinc@gmail.com>
2025-04-25 15:02:39 -0700

    refactor: moved static scanner to Main.java

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,11 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+
         new AccountingLedgerApp().start();
     }
 }
