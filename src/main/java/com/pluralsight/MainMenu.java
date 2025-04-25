b987f5a29fecc8c58c81797daaa5569d33529156
paulofranklins <paulofranklinc@gmail.com>
2025-04-25 15:04:34 -0700

    refactor: renamed Menu to MainMenu

---
+++ src/main/java/com/pluralsight/MainMenu.java
@@ -0,0 +1,39 @@
+package com.pluralsight;
+
+import static com.pluralsight.Main.scanner;
+
+public class MainMenu {
+    private final LedgerScreen ledgerScreen = new LedgerScreen();
+    private final ClearScreen clearScreen = new ClearScreen();
+
+    private String showMainMenu() {
+        System.out.println("What would you like to do?");
+        System.out.println("[D] Add Deposit");
+        System.out.println("[P] Make Payment");
+        System.out.println("[L] Ledger");
+        System.out.println("[X] Exit");
+
+        try {
+            System.out.print("Select an option: ");
+            return scanner.nextLine().trim().toUpperCase();
+        } finally {
+            clearScreen.clearScreen();
+        }
+    }
+
+    public void mainMenuLogic() {
+        while (true) {
+            var option = new MainMenu().showMainMenu();
+            switch (option) {
+                case "D" -> System.out.println("Add Deposit");
+                case "P" -> System.out.println("Make Payment");
+                case "L" -> ledgerScreen.ledgerMenuLogic();
+                case "X" -> {
+                    System.out.println("Thank you for using accounting ledger app");
+                    System.exit(0);
+                }
+                default -> System.out.println("Invalid option");
+            }
+        }
+    }
+}
