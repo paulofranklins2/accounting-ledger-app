34ed8e4fe680008695b04b6d88527cfccd7a28fb
paulofranklins <paulofranklinc@gmail.com>
2025-04-28 12:04:57 -0700

    refactor: Transaction Record to class

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,11 +1,40 @@
 package com.pluralsight;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-//public record Transaction(LocalDate date, LocalTime time, String description, String vendor, BigDecimal amount) {
-//}
+@Getter
+@Setter
+@NoArgsConstructor
+public class Transaction {
+    private String description;
+    private String vendor;
+    private String transactionType;
+    private BigDecimal amount;
+    private LocalDateTime timestamp;
+    private LocalDate transactionDate;
+    private LocalTime transactionTime;
 
-public record Transaction(String description, String vendor, BigDecimal amount) {
+    public Transaction(String description, String vendor, String transactionType, BigDecimal amount, LocalTime transactionTime, LocalDate transactionDate) {
+        this.description = description;
+        this.vendor = vendor;
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.transactionTime = transactionTime;
+        this.transactionDate = transactionDate;
+    }
+
+    public Transaction(String description, String vendor, String transactionType, BigDecimal amount) {
+        this.description = description;
+        this.vendor = vendor;
+        this.transactionType = transactionType;
+        this.amount = amount;
+    }
 }
