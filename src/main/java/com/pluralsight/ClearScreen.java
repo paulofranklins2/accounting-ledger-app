a0fa167df6a8cc5d205349ac67a7869a67aeb6ae
paulofranklins <paulofranklinc@gmail.com>
2025-04-29 10:25:42 -0700

    feat: add ClearScreen utility for improved console readability
    - Implemented `cleanPreviousScreen()` to simulate clearing the console by printing 50 new lines
    - Added `cleanLogScreen()` which prompts user input before clearing, useful for pausing after logs or actions

--- src/main/java/com/pluralsight/ClearScreen.java
+++ src/main/java/com/pluralsight/ClearScreen.java
@@ -1,9 +1,17 @@
 package com.pluralsight;
 
+import static com.pluralsight.InputHelper.stringInput;
+
 public class ClearScreen {
-    public void clearScreen() {
+
+    public void cleanPreviousScreen() {
         for (int i = 0; i < 50; i++) {
             System.out.println();
         }
+    }
+
+    public void cleanLogScreen() {
+        stringInput("Press any key to continue: ");
+        cleanPreviousScreen();
     }
 }
