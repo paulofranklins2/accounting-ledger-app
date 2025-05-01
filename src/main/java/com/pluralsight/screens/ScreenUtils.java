2e9760e5bc523db347613020606e2298af561c5b
paulofranklins <paulofranklinc@gmail.com>
2025-05-01 13:17:49 -0700

    refactor: replace ClearScreen with ScreenUtils utility class
    - Removed ClearScreen class
    - Added ScreenUtils with clearConsole and pauseAndClearScreen methods
    - Centralized screen handling logic for reuse across application

--- src/main/java/com/pluralsight/screens/ScreenUtils.java
+++ src/main/java/com/pluralsight/screens/ScreenUtils.java
@@ -2,16 +2,16 @@ package com.pluralsight.screens;
 
 import static com.pluralsight.services.InputHelper.stringInput;
 
-public class ClearScreen {
+public class ScreenUtils {
 
-    public void cleanPreviousScreen() {
+    public void clearConsole() {
         for (int i = 0; i < 50; i++) {
             System.out.println();
         }
     }
 
-    public void cleanLogScreen() {
+    public void pauseAndClearScreen() {
         stringInput("\nPress ENTER to continue: ");
-        cleanPreviousScreen();
+        clearConsole();
     }
 }
