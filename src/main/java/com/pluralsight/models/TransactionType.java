d61fb919d5c4964238a80954ff1e95409f54869b
paulofranklins <paulofranklinc@gmail.com>
2025-05-01 14:40:12 -0700

    feat: add fromValue method to TransactionType enum
    - Added static fromValue(String value) method to TransactionType to convert enum value to formatted name (e.g., "D" -> "Deposit")

--- src/main/java/com/pluralsight/models/TransactionType.java
+++ src/main/java/com/pluralsight/models/TransactionType.java
@@ -14,4 +14,14 @@ public enum TransactionType {
     TransactionType(String description) {
         this.value = description;
     }
+
+    public static String fromValue(String value) {
+        for (TransactionType type : TransactionType.values()) {
+            if (type.getValue().equalsIgnoreCase(value)) {
+                String name = type.name().toLowerCase();
+                return name.substring(0, 1).toUpperCase() + name.substring(1);
+            }
+        }
+        return null;
+    }
 }
